72a23931bb048dda25eedf054ba7937dcf5e74e4
benjamin_fan <benjamin_fan@trendmicro.com>
2021-06-24 21:44:42 +0800

    feat(weatherforecast): add Api description and parameter description for nswagger
    Use OpenApiOperation attribute to add Api description and description atterbute for parameter.

--- AspNetCoreSerilog/Controllers/WeatherForecastController.cs
+++ AspNetCoreSerilog/Controllers/WeatherForecastController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NSwag.Annotations;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,6 +38,26 @@ namespace AspNetCoreSerilog.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("{days}")]
+        [OpenApiOperation("This is operation summary", "This is operation description")]
+        [ProducesResponseType(typeof(WeatherForecast), 200)]
+        public IEnumerable<WeatherForecast> Get(
+            [Required][FromRoute][Description("days that will show")]
+            int days)
+        {
+
+            _logger.LogInformation("star to process action");
+            var rng = new Random();
+            return Enumerable.Range(1, days).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
         }
     }
 }
